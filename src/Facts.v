87ebcc7b373ae57330eae9df63e93ef2940298f0
Langston Barrett <langston.barrett@gmail.com>
2017-05-14 04:00:55 +0000

    simplify Facts

--- src/Facts.v
+++ src/Facts.v
@@ -6,21 +6,9 @@ Require Import MathClasses.interfaces.vectorspace.
 Require Import MathClasses.orders.dec_fields.
 
 Section Facts.
-  Context `{@SemiNormedSpace
-              K V
-              Ke Kle Kzero Knegate Kabs Vnorm Ke Kplus Kmult Kzero Kone
-              Knegate Krecip Ve Vop Vunit Vnegate smkv
-           }.
-  Context `{@SemiNormedSpace
-              K W1
-              Ke Kle Kzero Knegate Kabs Wnorm1 Ke Kplus Kmult Kzero Kone
-              Knegate Krecip We1 Wop1 Wunit1 Wnegate1 smkw1
-           }.
-  Context `{@SemiNormedSpace
-              K W2
-              Ke Kle Kzero Knegate Kabs Wnorm2 Ke Kplus Kmult Kzero Kone
-              Knegate Krecip We2 Wop2 Wunit2 Wnegate2 smkw2
-           }.
+  Context `{SemiNormedSpace K V}.
+  Context `{SemiNormedSpace K W1}.
+  Context `{SemiNormedSpace K W2}.
   Context `{!FullPseudoSemiRingOrder Kle Klt}.
   Context `{forall x y : K, Decision (x = y)}.
 
@@ -36,7 +24,7 @@ Section Facts.
       (* Unfurl our hypothesis *)
       intros f_O_g.
       unfold big_O in *.
-      destruct f_O_g as [k [zero_lt_k [n0 [zero_lt_n0 f_O_g]]]].
+      destruct f_O_g as [k [zero_lt_k [m0 [zero_lt_m0 f_O_g]]]].
 
       unfold big_Omega.
       exists (/k).
@@ -45,16 +33,16 @@ Section Facts.
         now apply MathClasses.orders.dec_fields.pos_dec_recip_compat.
       }
       {
-        exists n0.
+        exists m0.
         split; try assumption.
-        intros n n0_le_n.
-        assert (∥ f n ∥ ≤ k * (∥ g n ∥)) by (now apply f_O_g).
+        intros m n0_le_n.
+        assert (∥ f m ∥ ≤ k * (∥ g m ∥)) by (now apply f_O_g).
 
         assert (inv_k_gt_0 : 0 < / k) by
             (now apply MathClasses.orders.dec_fields.pos_dec_recip_compat).
         assert (mult_inv_eq_1 : (/ k) * k = 1) by
             (now apply dec_recip_inverse_ge_0).
-        setoid_replace (∥g n∥) with (1 * ∥g n∥) by (now rewrite left_identity).
+        setoid_replace (∥g m∥) with (1 * ∥g m∥) by (now rewrite left_identity).
         (* This is awkward, but causes Coq not to automagically multiply
             (/ k_O) * k_O *)
         rewrite <- mult_inv_eq_1.
@@ -69,7 +57,7 @@ Section Facts.
 
       (* Unfurl our hypothesis *)
       unfold big_Omega in *.
-      destruct g_Ω_f as [k [zero_lt_k [n0 [zero_lt_n0 g_Ω_f]]]].
+      destruct g_Ω_f as [k [zero_lt_k [m0 [zero_lt_m0 g_Ω_f]]]].
 
       unfold big_O.
       exists (/k).
@@ -78,16 +66,16 @@ Section Facts.
         now apply MathClasses.orders.dec_fields.pos_dec_recip_compat.
       }
       {
-        exists n0.
+        exists m0.
         split; try assumption.
-        intros n n0_le_n.
-        assert (k * (∥ f n ∥) ≤ ∥ g n ∥) by (now apply g_Ω_f).
+        intros m n0_le_n.
+        assert (k * (∥ f m ∥) ≤ ∥ g m ∥) by (now apply g_Ω_f).
 
         assert (inv_k_gt_0 : 0 < / k) by
             (now apply MathClasses.orders.dec_fields.pos_dec_recip_compat).
         assert (mult_inv_eq_1 : (/ k) * k = 1) by
             (now apply dec_recip_inverse_ge_0).
-        setoid_replace (∥f n∥) with (1 * ∥f n∥) by (now rewrite left_identity).
+        setoid_replace (∥f m∥) with (1 * ∥f m∥) by (now rewrite left_identity).
         (* This is awkward, but causes Coq not to automagically multiply
             (/ k_O) * k_O *)
         rewrite <- mult_inv_eq_1.
@@ -108,22 +96,22 @@ Section Facts.
 
     exists 1.
     split; try (apply zero_lt_one_dec).
-    destruct (f_o_g 1 zero_lt_one_dec) as [n0 [zero_lt_n0 ?]].
-    exists n0.
+    destruct (f_o_g 1 zero_lt_one_dec) as [m0 [zero_lt_m0 ?]].
+    exists m0.
     split; assumption.
   Qed.
 
   (**
    Little ω is strictly more stringent than big Ω.
    *)
-  Lemma omega_implies_Omega : ∀ (f : V → W1) (g : V → W2), f ∈ ω(g) -> f ∈ Ω(g).
+  Lemma omega_implies_Omega : ∀ (f : V → W1) (g : V → W2), f ∈ ω(g) → f ∈ Ω(g).
     intros f g f_ω_g.
     unfold big_Omega.
 
     exists 1.
     split; try (apply zero_lt_one_dec).
-    destruct (f_ω_g 1 zero_lt_one_dec) as [n0 [zero_lt_n0 ?]].
-    exists n0.
+    destruct (f_ω_g 1 zero_lt_one_dec) as [m0 [zero_lt_m0 ?]].
+    exists m0.
     split; assumption.
   Qed.
 
